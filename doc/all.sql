6d2b69587e35436fddc9d2082070be3bf44bcd1f
arceus <zhij311@gmail.com>
2021-03-05 02:31:32 -0500

    3-5 Mybatis auto generator

--- doc/all.sql
+++ doc/all.sql
@@ -1,5 +1,4 @@
 drop table if exists `test`;
-
 create  table `test`(
     `id` bigint not null comment 'id',
     `name` varchar(50) comment 'name',
@@ -10,3 +9,13 @@ create  table `test`(
 SELECT * From `test`;
 
 insert into `test` (id, name, password) VALUES (0,'test','pass');
+
+#demo table for mybatis generator
+create  table `demo`(
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment 'name',
+                        primary key (`id`)
+
+)engine=innodb default  charset =utf8mb4;
+
+insert into `demo` (id, name) VALUES (0,'test');
