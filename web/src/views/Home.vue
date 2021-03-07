bbe4c2d40ac02f5f2707fae128b33b7657b0c0b0
arceus <zhij311@gmail.com>
2021-03-07 00:58:39 -0500

    4-5 Vue3 Integrate Ant Design

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Button type="primary">Primary Button</Button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
