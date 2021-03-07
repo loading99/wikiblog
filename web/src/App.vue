ac53e99afe3a5c91aa95adb28e663cc2b9afa62c
arceus <zhij311@gmail.com>
2021-03-07 02:05:54 -0500

    4-6.5 HomePage Design

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+          theme="dark"
+          mode="horizontal"
+          v-model:selectedKeys="selectedKeys1"
+          :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">nav 10</a-menu-item>
+        <a-menu-item key="2">nav 20</a-menu-item>
+        <a-menu-item key="3">nav 30</a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+
+      <router-view/>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
-
+<script lang="ts">
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  },
+  setup() {
+    return {
+      selectedKeys1: ref<string[]>(['2']),
+      selectedKeys2: ref<string[]>(['1']),
+      openKeys: ref<string[]>(['sub1']),
+    };
+  },
+});
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#components-layout-demo-top-side .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
 }
 
-#nav {
-  padding: 30px;
+.ant-row-rtl #components-layout-demo-top-side .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.site-layout-background {
+  background: #fff;
 }
 </style>
