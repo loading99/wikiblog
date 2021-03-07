9ab391970b70ef82e50b8608ff7fab4d72f45be6
arceus <zhij311@gmail.com>
2021-03-07 04:49:37 -0500

    4-6.10 CSS animation

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
+<div class="background">
+
+  <div class="content">
+    <small>Welcome!</small>
     <h1>This is an about page</h1>
   </div>
+  <div class="snowflake">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+    <img src="../assets/snowflake1.png">
+  </div>
+</div>
 </template>
+
+<style>
+@import "about.css";
+</style>
