7a20f60675fe9c121cbaeec5aa8ab4b26feacb41
arceus <zhij311@gmail.com>
2021-03-08 23:02:20 -0500

    5-9 add AOP

--- src/main/java/com/jiawa/wiki/config/SpringMVCConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMVCConfig.java
@@ -1,21 +1,21 @@
-package com.jiawa.wiki.config;
-
-
-import com.jiawa.wiki.utils.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMVCConfig implements WebMvcConfigurer {
-    @Resource
-    LogInterceptor Interceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(Interceptor)
-                .addPathPatterns("/**");
-
-    }
-}
+//package com.jiawa.wiki.config;
+//
+//
+//import com.jiawa.wiki.utils.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMVCConfig implements WebMvcConfigurer {
+//    @Resource
+//    LogInterceptor Interceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(Interceptor)
+//                .addPathPatterns("/**");
+//
+//    }
+//}
--- src/main/java/com/jiawa/wiki/utils/LogFilter.java
+++ src/main/java/com/jiawa/wiki/utils/LogFilter.java
@@ -1,33 +1,33 @@
- package com.jiawa.wiki.utils;
-
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.stereotype.Component;
-
- import javax.servlet.*;
- import javax.servlet.http.HttpServletRequest;
- import java.io.IOException;
-
- @Component
- public class LogFilter implements Filter {
-
-     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
-
-     }
-
-     @Override
-     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-         // 打印请求信息
-         HttpServletRequest request = (HttpServletRequest) servletRequest;
-         LOG.info("------------- LogFilter starts -------------");
-         LOG.info("Request url: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("Remote url: {}", request.getRemoteAddr());
-
-         long startTime = System.currentTimeMillis();
-         filterChain.doFilter(servletRequest, servletResponse);
-         LOG.info("------------- LogFilter ends time：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+// package com.jiawa.wiki.utils;
+//
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
+//
+// import javax.servlet.*;
+// import javax.servlet.http.HttpServletRequest;
+// import java.io.IOException;
+//
+// @Component
+// public class LogFilter implements Filter {
+//
+//     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//     @Override
+//     public void init(FilterConfig filterConfig) throws ServletException {
+//
+//     }
+//
+//     @Override
+//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//         // 打印请求信息
+//         HttpServletRequest request = (HttpServletRequest) servletRequest;
+//         LOG.info("------------- LogFilter starts -------------");
+//         LOG.info("Request url: {} {}", request.getRequestURL().toString(), request.getMethod());
+//         LOG.info("Remote url: {}", request.getRemoteAddr());
+//
+//         long startTime = System.currentTimeMillis();
+//         filterChain.doFilter(servletRequest, servletResponse);
+//         LOG.info("------------- LogFilter ends time：{} ms -------------", System.currentTimeMillis() - startTime);
+//     }
+// }
